1e080e26700d76b6280bef54a912f40e86dac3c4
hoangdang2000PR <hoangdang462000@gmail.com>
2024-10-06 18:29:45 +0700

    Create Config.js

---
+++ Config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Babe à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích em🥰 <33',
+    btnNo: 'Không, Bạn thôi à :3',
+    question:'Trên thế giới hơn 8 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'mình thích cậu đó <33333333',
+    mess: 'Tớ biết mà 🥰. mình cũng thích cậu 😘😘',
+    messDesc: 'Làm người yêu tớ nha🥰.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://fb.com'. https://www.facebook.com/profile.php?id=100059947774800&mibextid=ZbWKwL
+}
